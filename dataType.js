05f891bdd75d05f755e4686e6423c71ccd8e2922
PanupongJJ <panupong.jangjun@gmail.com>
2025-05-05 22:56:49 +1000

    fix

--- dataType.js
+++ dataType.js
@@ -5,7 +5,7 @@ String ==> "" '' ``
 Boolean ==> true false
 number ==> 10  334.4  -43.3 
 
-undefined ==> u haven't assinged the value yet but will do later on
-null ==> u want to set somthings as "NOT THING" or "Empty"
+undefined ==> u haven't assigned the value yet but will do later on
+null ==> u want to set somethings as "NOT THING" or "Empty"
 
 */
