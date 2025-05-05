103bf8d7bcabbed244090a4d874f9d21a98b6386
PanupongJJ <panupong.jangjun@gmail.com>
2025-05-05 22:29:45 +1000

    data type

---
+++ dataType.js
@@ -0,0 +1,11 @@
+
+
+/* 
+String ==> "" '' ``
+Boolean ==> true false
+number ==> 10  334.4  -43.3 
+
+undefined ==> u haven't assinged the value yet but will do later on
+null ==> u want to set somthings as "NOT THING" or "Empty"
+
+*/
